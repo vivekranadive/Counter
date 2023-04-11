49956088513d67c0a47a80a937a2fec05e6d04a4
vivekchittepu <vivek@minminicorp.com>
2023-04-11 01:30:44 -0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import clickSound from './sound.mp3';
 
 function App() {
+  const [count, setCount] = useState(
+    Number(localStorage.getItem('count') || 0)
+  );
+
+  useEffect(() => {
+    localStorage.setItem('count', count);
+  }, [count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    const audio = new Audio(clickSound);
+    audio.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">TURN YOUR SOUND UP!</h1>
+      <h1 className="title">Count: {count}</h1>
+      <button className="btn" onClick={handleClick}>
+        <span>Click me!</span>
+      </button>
     </div>
   );
 }
